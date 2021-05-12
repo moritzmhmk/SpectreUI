e38cb6f7f21704e23b62a6ed8a9e3e47b6ed1692
moritzmhmk <moritzmhmk@gmail.com>
2021-05-12 21:04:21 +0200

    Initial Commit

---
+++ SpectreUI/SpectreUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpectreUIApp.swift
+//  SpectreUI
+//
+//  Created by Moritz Kanemeier on 12.05.21.
+//
+
+import SwiftUI
+
+@main
+struct SpectreUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
